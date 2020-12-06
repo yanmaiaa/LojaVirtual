5ce4346d34d016235df013d10b4860680fce85fe
Yan Maia <yandamasceno01@gmail.com>
2020-12-06 01:07:08 -0300

    feat: creating first components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Produtos from "./components/Produtos";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Produtos />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./Header.module.css";
+
+const Header = () => {
+  return <div></div>;
+};
+
+export default Header;
