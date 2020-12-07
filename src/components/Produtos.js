12c112eed098204466560f32685b4565e0873457
Yan Maia <yandamasceno01@gmail.com>
2020-12-07 02:30:19 -0300

    fix: solving the map bug

--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -6,7 +6,7 @@ const Produtos = () => {
   const [produtos, setProdutos] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("https://ranekapi.origamid.dev/json/api/produto/notebook")
+    fetch("https://ranekapi.origamid.dev/json/api/produto")
       .then((r) => r.json())
       .then((json) => setProdutos(json));
   }, []);
@@ -17,7 +17,7 @@ const Produtos = () => {
     <section className={styles.produtos + " animeLeft"}>
       {produtos.map((produto) => (
         <Link to={`produto/${produto.id}`} key={produto.id}>
-          <img src={produto.fotos[0].src} alt={produto.foto[0].titulo} />
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
           <h1 className={styles.nome}>{produto.nome}</h1>
         </Link>
       ))}
