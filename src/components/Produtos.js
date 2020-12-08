a371524a3a6e893121f878f8b582a13f72e60ba1
Yan Maia <yandamasceno01@gmail.com>
2020-12-08 01:58:27 -0300

    adding head on page

--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Head from "./Head";
 import styles from "./Produtos.module.css";
 
 const Produtos = () => {
@@ -14,6 +15,7 @@ const Produtos = () => {
   if (produtos === null) return null;
   return (
     <section className={styles.produtos + " animeLeft"}>
+      <Head title="LojaVirtual" description="Compre já os seus produtos" />
       {produtos.map((produto) => (
         <Link to={`produto/${produto.id}`} key={produto.id}>
           <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
