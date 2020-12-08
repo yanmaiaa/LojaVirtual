8f7688630f386e8791761252ee5d6cfee9d80939
Yan Maia <yandamasceno01@gmail.com>
2020-12-08 01:54:18 -0300

    feat: building Produto page

--- src/components/Produto.js
+++ src/components/Produto.js
@@ -1,25 +1,48 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Head from "./Head";
 import styles from "./Produto.module.css";
 
 const Produto = () => {
-  const [Produto, setProduto] = React.useState(null);
+  const [produto, setProduto] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
-  const [erro, setErro] = React.useState(null);
+  const [error, setError] = React.useState(null);
   const { id } = useParams();
 
   React.useEffect(() => {
     async function fetchProduto(url) {
-      const response = await fetch(url);
-      const json = response.json();
-      setProduto(json);
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        const json = await response.json();
+        setProduto(json);
+      } catch (erro) {
+        setError("Um erro ocorreu");
+      } finally {
+        setLoading(false);
+      }
     }
     fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
   }, [id]);
+
+  if (loading) return <div>Carregando...</div>;
+  if (error) return <p>{error}</p>;
+  if (produto === null) return null;
   return (
-    <div className={styles.produto}>
-      <h1>Produto</h1>
-    </div>
+    <section className={styles.produto + " animeLeft"}>
+      <Head
+        title={` LojaVirtual | ${produto.nome}`}
+        description={` LojaVirtual | Escolha o seu produto: ${produto.nome}`}
+      />
+      {produto.fotos.map((foto) => (
+        <img key={foto.src} src={foto.src} alt={foto.titulo} />
+      ))}
+      <div>
+        <h1>{produto.nome}</h1>
+        <span className={styles.preco}>{produto.preco}</span>
+      </div>
+      <p className={produto.descricao}>{produto.descricao}</p>
+    </section>
   );
 };
 
